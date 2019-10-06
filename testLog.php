23c20a478ac877cc8a3e1a710585d4818207356f
Omar <oai4@njit.edu>
2019-10-06 02:25:31 -0400

    Made logger script and test

---
+++ testLog.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+
+//testLog.php
+
+require_once("Log.php.inc");
+
+$l = new iLog("./_logs/testLog.log","w");
+
+$l->print("Testing log system...");
+$l->done();
+$l->print("Testing empty line...");
+$l->print();
+$l->print();
+$l->done();
+$l->close();
+?>

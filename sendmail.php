9c0793532061ddb555eb43619ba271f688c62604
anwar <ars66@njit.edu>
2019-10-24 20:09:54 -0400

    distributed login

--- sendmail.php
+++ sendmail.php
@@ -1,7 +1,7 @@
 <?php
    $to_email = "entermatrixxx@gmail.com";
    $subject = "Simple Email Test via PHP";
-   $body = "Hi,nn This is test email send by PHP Script";
+   $body = "Hi, This is test email send by PHP Script";
    $headers = "From: entermatrixxx@gmail.com";
  
    if ( mail($to_email, $subject, $body, $headers)) {

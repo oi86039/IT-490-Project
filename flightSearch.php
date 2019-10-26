bd5b3d2dcc9d5bb1e6f9dd6f49c4d6584ba7dd56
Omar <oai4@njit.edu>
2019-10-26 19:37:31 -0400

    Backend webpages almost working. Need to retest!

--- flightSearch.php
+++ flightSearch.php
@@ -1,4 +1,24 @@
-#!/usr/bin/php
+<!DOCTYPE html>
+<meta charset="utf-8"/>
+
+<style>
+  .F1 {   width: 60% ; Background: white ;   margin:auto   }             
+</style>
+
+<form  action="./IT-490/destSearch.php">
+<fieldset class="F1">
+  <legend>Select Origin/Destination</legend>
+
+<!--Type Place ID here-->
+<span id = "PlaceID">
+Enter Place ID of desired origin: <input type= text name="PlaceID" id= "PlaceID" placeholder="Enter PlaceID:"  autocomplete=off> <br>
+Enter Place ID of desired destination: <input type= text name= "PlaceID" id="PlaceID" placeholder="Enter PlaceID:" autocomplete = off> <br><br>
+</span>
+
+
+Origin <br>
+Place ID &#9;|&#9; Place Name &#9;|&#9; Country ID &#9;|&#9; RegionId &#9;|&#9; City ID &#9;|&#9; Country Name <br><br>
+
 <?php
 require_once('path.inc');
 require_once('get_host_info.inc');
@@ -49,15 +69,52 @@ $l->print("Request VarDump:\n\n");
 //$l->print($request);
 $l->print();
 
+//Get response
 $response = $client->send_request($request);
 //$response = $client->publish($request);
 
-echo "client received response: ".PHP_EOL;
+$l->print("Client received response: ".PHP_EOL);
+
+//var_dump($response);
+
+//$response = json_decode($response,true);
+
+//var_dump($response);
+$l->print("\n\n");
+
 var_dump($response);
-echo "\n\n";
+
+//echo ("PROJECTED VALUE:". $response["Places"][0]["PlaceId"]."\n");
+
+//Display Origin
+for($i = 0; $i < count($response[0]["Places"]); $i++){
+	$PlaceID = $response[0]["Places"][$i]["PlaceId"];
+	$PlaceName = $response[0]["Places"][$i]["PlaceName"];
+	$CountryId = $response[0]["Places"][$i]["CountryId"];
+	$RegionID = $response[0]["Places"][$i]["RegionId"];
+	$CityID = $response[0]["Places"][$i]["PlaceId"];
+	$CountryName = $response[0]["Places"][$i]["CountryName"];
+	echo "<br>    ". $PlaceID."&#9;|&#9;".$PlaceName."&#9;|&#9;".$CountryId."&#9;|&#9;".$RegionID."&#9;|&#9;".$CityID."&#9;|&#9;".$CountryName."<br><br>";
+}
+
+echo "<br>Destination <br>
+Place ID \t|\t Place Name \t|\t Country ID \t|\t; RegionId \t|\t City ID \t|\t Country Name <br><br>
+";
+
+//Display Destination
+for($i = 0; $i < count($response[1]["Places"]); $i++){
+	$PlaceID = $response[1]["Places"][$i]["PlaceId"];
+	$PlaceName = $response[1]["Places"][$i]["PlaceName"];
+	$CountryId = $response[1]["Places"][$i]["CountryId"];
+	$RegionID = $response[1]["Places"][$i]["RegionId"];
+	$CityID = $response[1]["Places"][$i]["PlaceId"];
+	$CountryName = $response[1]["Places"][$i]["CountryName"];
+	echo "<br>    ". $PlaceID."&#9;|&#9;".$PlaceName."&#9;|&#9;".$CountryId."&#9;|&#9;".$RegionID."&#9;|&#9;".$CityID."&#9;|&#9;".$CountryName."<br><br>";
+}
 
 //CLose Logger
 $l->sendToRabbitMQ(__DIR__ . '/_logs/flightSearch.log','./_logs/flightSearch.log');
 
-echo "Flight Search.php"." END".PHP_EOL;
-
+//echo "Flight Search.php"." END".PHP_EOL;
+?>
+</fieldset>

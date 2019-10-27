33fe360431f469bcc81989920a16a77c114ccb22
anwar <ars66@njit.edu>
2019-10-26 21:18:12 -0400

    adding email still needs work

--- sendmail.php
+++ sendmail.php
@@ -1,12 +1,17 @@
 <?php
-   $to_email = "njitit490@gmail.com";
-   $subject = "Simple Email Test via PHP";
-   $body = "Hi, This is test email send by PHP Script";
+
+function email($to, $sub, $body) {
+
+  # $to_email = "njitit490@gmail.com";
+  # $subject = "Simple Email Test via PHP";
+  # $body = "Hi, This is test email send by PHP Script";
    $headers = "From: njitit490@gmail.com";
- 
-   if ( mail($to_email, $subject, $body, $headers)) {
-      echo("Email successfully sent to $to_email...");
+
+   if ( mail($to, $sub, $body, $headers)) {
+      echo("Email successfully sent to $to...");
    } else {
       echo("Email sending failed...");
    }
+   }
 ?>
+

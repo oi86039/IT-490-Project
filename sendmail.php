e719b56e60e7bf9d3f7ac5b8f942be7d5cddc14b
anwar <ars66@njit.edu>
2019-10-26 21:47:48 -0400

    registration to db table works

--- sendmail.php
+++ sendmail.php
@@ -1,11 +1,11 @@
 <?php
 
 function email($to, $sub, $body) {
-
+echo " function called";
   # $to_email = "njitit490@gmail.com";
   # $subject = "Simple Email Test via PHP";
   # $body = "Hi, This is test email send by PHP Script";
-   $headers = "From: njitit490@gmail.com";
+   $headers="From: njitit490@gmail.com";
 
    if ( mail($to, $sub, $body, $headers)) {
       echo("Email successfully sent to $to...");

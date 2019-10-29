644704a1b2b5596b9bb6d9f8cb5645d76f3c05b3
hsantos2818 <hs487@njit.edu>
2019-10-29 17:50:10 -0400

    fixed log error fixed api error

--- f2bserver_api.php
+++ f2bserver_api.php
@@ -5,13 +5,13 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 require_once('login.php.inc');
-require_once('Log.php.inc');
-
-//$L = new iLog("./backend_log.log","a");
+require('Log.php.inc');
 
 function getPlaces($query,$country,$currency,$locale){
-	//global $L;
-	//$L -> print("'getPlaces' called for proper syntax");
+	global $L;
+	$L -> print("'getPlaces' called for proper syntax");
+	echo "\n";
+
 	echo "query: ".$query."\n";	
 	$curl = curl_init();
 
@@ -34,11 +34,12 @@ function getPlaces($query,$country,$currency,$locale){
 
 	$response = curl_exec($curl);
 	$err = curl_error($curl);
-
+	
 	curl_close($curl);
 
 	if ($err) {
-		//$L -> print("ERROR: in 'getPlaces' function");
+		$L -> print("ERROR: in 'getPlaces' function");
+		echo "\n";
 		echo "cURL Error #:" . $err;
 	} else {
 		$jresponse = json_decode($response,true);
@@ -48,8 +49,9 @@ function getPlaces($query,$country,$currency,$locale){
 	}
 }
 function setSession($country,$currency,$locale,$origin,$dest,$leaveDate,$adults,$tags){
-	//global $L;
-	//$L -> print("'setSession' called");
+	global $L;
+	$L -> print("'setSession' called");
+	echo "\n";
 	
 	echo "sS: Origin is ".$origin."\n";
 	echo "sS: Destination is ".$dest."\n";
@@ -106,21 +108,41 @@ function setSession($country,$currency,$locale,$origin,$dest,$leaveDate,$adults,
 	
 	if ($err) {
 		echo "cURL Error #:" . $err;
-		//$L -> print("ERROR: in 'setSession' function");
+		$L -> print("ERROR: in 'setSession' function");
+		echo "\n";
 	} else {
 		echo "Session key created: ".$locKey."\n";
 		return $locKey;
 	}
 }
 function getSession($locKey, $filters){
-	//global $L;
-	//$L -> print("'getSession' is called");
+	global $L;
+	$L -> print("'getSession' is called");
+	echo "\n";
 	
 	$curl = curl_init();
 
+	$urlF = "";
+
+	$c = 0;
+
+	if(count($filters) != 0){
+                foreach($tags as $key => $t){
+			$c++;
+			if($t != "" && $c < count($filters)){
+                                $urlF .= $key."=".$t."&";
+			}
+			else{
+				$urlF .= $key."=".$t;
+			}
+                }
+                echo $urlF." FOR FILTERS"."\n";
+        }
+
+
 	while(true){
 		curl_setopt_array($curl, array(
-			CURLOPT_URL => "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/$locKey?pageIndex=0&pageSize=10",
+			CURLOPT_URL => "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/$locKey?$urlF",
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_FOLLOWLOCATION => true,
 			CURLOPT_ENCODING => "",
@@ -146,18 +168,84 @@ function getSession($locKey, $filters){
 	
 	if ($err) {
 		echo "cURL Error #:" . $err;
-		//$L -> print("ERROR: in 'getSession' function");
+		$L -> print("ERROR: in 'getSession' function");
+		echo "\n";
 	} else {
 		//var_dump($jresponse);
-		var_dump($jresponse["Query"]);
-                return $jresponse;
+		var_dump($jresponse['Query']);
+
+		$jr_itineraries = $jresponse["Itineraries"];
+		$jr_legs = $jresponse['Legs'];
+		$jr_segments = $jresponse['Segments'];
+		$jr_carriers = $jresponse['Carriers'];
+		$jr_agents = $jresponse['Agents'];
+		$jr_places = $jresponse['Places'];
+		$jr_currencies = $jresponse['Currencies'];
+
+		foreach($jr_itineraries as $key => $i){ //intineraries => [0]
+			foreach($i as $key2 => $i2){ //[0] => {}
+				if($key2 == "OutboundLegId"){ //OutboundLegId => 123412341243
+					foreach($jr_legs as $key3 => $i3){
+						foreach($i3 as $key4 => $i4){
+							if($i3["Id"] == $i2){
+								$jresponse["Itineraries"][$key][$key2] = $i3;
+							}
+							else{
+								break;
+							}
+						}
+					}
+				}
+				if($key2 == "InboundLegId"){ //InboundLegId => 243523452543
+                                        foreach($jr_legs as $key3 => $i3){
+                                                foreach($i3 as $key4 => $i4){
+                                                        if($i3["Id"] == $i2){
+                                                                $jresponse["Itineraries"][$key][$key2] = $i3;
+                                                        }
+                                                        else{
+                                                                break;
+                                                        }
+                                                }
+                                        }
+				}
+				if($key2 == "PriceOptions"){ //PriceOptions => [0]
+					foreach($i2 as $key3 => $i3){ //[0] => {}
+						foreach($i3 as $key4 => $i4){ //Agents => {}
+							if($key4 == "Agents"){
+								foreach($i4 as $key5 => $i5){ //0 => 43234
+									foreach($jr_agents as $akey => $ai){//(dict)Agents => [0]
+										foreach($ai as $akey2 => $ai2){//[0] => {}
+											foreach($ai2 as $akey3 => $ai3){//Id => 43234
+												if($akey3 == "Id"){
+													if($i5 == $ai2["Id"]){
+														$jrequest[Itineraries][$key][$key2][$key3][$key4][$key5] = $ai2;
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+		$final_jr = $jresponse["Itineraries"];
+		var_dump($final_jr);
+
+		//return $jresponse;
+		return $final_jr;
+
 	}
 }
 function requestProcessor($request){
-	//global $L;
+	global $L;
 	static $origin = 0;
 	//Required Params: query, country, currency, locale, originPlace, destinationPlace, outboundDate, adults, tags[], filters[].
-	//$L -> print("Request received from Front-End");
+	$L -> print("Request received from Front-End");
+	echo "\n";
 
 	//array should have: ()
 
@@ -165,20 +253,38 @@ function requestProcessor($request){
 	var_dump($request);
 	if(!isset($request['type']))
 	{
-		//$L -> print("ERROR: unsupported message type");
+		$L -> print("ERROR: unsupported message type");
+		echo "\n";
 		return "ERROR: unsupported message type";
   	}
   	switch ($request['type'])
   	{
 		case "getPlaces":
-			$setPlaces = array(getPlaces($request['originPlace'],$request['country'],$request['currency'],$request['locale']),getPlaces($request['destinationPlace'],$request['country'],$request['currency'],$request['locale']));
+			$setPlaces = array(
+				getPlaces($request['originPlace'],
+					$request['country'],
+					$request['currency'],
+					$request['locale']),
+				getPlaces($request['destinationPlace'],
+				$request['country'],
+				$request['currency'],
+				$request['locale'])
+			);
 			return $setPlaces;
 			break;
 		case "getSessions":
-			$locKey = setSession($request['country'],$request['currency'],$request['locale'],$request['originPlace'],$request['destinationPlace'],$request['outboundDate'],$request['adults'],$request['tags'],$request['filters']);
+			$locKey = setSession($request['country'],
+				$request['currency'],
+				$request['locale'],
+				$request['originPlace'],
+				$request['destinationPlace'],
+				$request['outboundDate'],
+				$request['adults'],
+				$request['tags'],
+				$request['filters']);
 			
-			//$L -> print("Returned setSession to FE");
-			
+			$L -> print("Returned setSession to FE");
+			echo "\n";
 			//**tag must include: 
 			//	inboundDate,
 			//	cabinClass,
@@ -188,8 +294,8 @@ function requestProcessor($request){
 			//	excludeCarriers,
 			//	groupPricing
 		
-			//$L -> print("Returned getSession to FE");
-			
+			$L -> print("Returned getSession to FE");
+			echo "\n";
 			return getSession($locKey,$request['filters']);
 			break;
 			//**filter must have:
@@ -217,15 +323,16 @@ function requestProcessor($request){
   	return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
 
+$L = new iLog("./backend_log.log","a");
 
-//$L = new iLog("./backend_log.log","a");
-
-//$L -> print("Listening session created");
-
+$L -> print("Listening session created");
+echo "\n";
 $server = new rabbitMQServer("frontToBack.ini","frontToBack");
 
 $server->process_requests('requestProcessor');
-//$L -> sentToRabbitMQ("./backend_log.log","./_logs/BackEnd.log");
+
+$L -> sendToRabbitMQ("./backend_log.log","./_logs/BackEnd.log");
+
 exit();
 ?>
 
